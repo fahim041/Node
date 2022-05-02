6eb18748ea0faece66f9c0a92ac7417dab3ed4ba
M0nster92 <fahimalsafi@gmail.com>
2022-05-01 21:23:36 -0400

    handling missing events from event bus

--- microservices/blog/event-bus/index.js
+++ microservices/blog/event-bus/index.js
@@ -6,12 +6,23 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
   axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4003/events", event);
+
+  res.send({ status: "ok" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => console.log("server is listening on port 4005"));
