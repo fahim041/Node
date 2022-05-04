e6b1ede19c5c33f0743d4d6b776f64d89f625b63
M0nster92 <fahimalsafi@gmail.com>
2022-05-03 22:37:22 -0400

    added docker images & kubectl's service & deployment

--- microservices/blog/posts/index.js
+++ microservices/blog/posts/index.js
@@ -41,4 +41,7 @@ app.post("/events", (req, res) => {
   res.send({});
 });
 
-app.listen(4000, () => console.log("Listening on port 4000"));
+app.listen(4000, () => {
+  console.log("latest version!!!");
+  console.log("Listening on port 4000");
+});
