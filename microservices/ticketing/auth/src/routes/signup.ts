f50b15f187acc4b34ca155460a3a3d99f3a2892b
M0nster92 <fahimalsafi@gmail.com>
2022-05-11 22:29:26 -0400

    password class

--- microservices/ticketing/auth/src/routes/signup.ts
+++ microservices/ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from 'express-validator';
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -16,11 +17,16 @@ router.post('/api/users/signup', [
     }
 
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email })
 
-    console.log('Creating a user');
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+        throw new BadRequestError('Email in use')
+    }
 
-    res.send({})
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(200).send(user);
 
 });
 
