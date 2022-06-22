eb813f1df24fd9e9dec452334175bd621a134e39
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2022-06-21 21:17:08 -0400

    sync

--- cmd/index.js
+++
@@ -1,8 +0,0 @@
-const Event = require("events");
-const emitter = new Event();
-
-emitter.on("messageLogged", (args) => {
-  console.log("listener called", args);
-});
-
-emitter.emit("messageLogged", "user");
