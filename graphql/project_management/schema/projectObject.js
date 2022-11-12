26e181956ad4f116a8a7d01ba6b9d8ac4b0dc403
Fahim Al Sami <46305384+M0nster92@users.noreply.github.com>
2022-11-12 05:11:12 +0000

    structure the graphql schema

---
+++ graphql/project_management/schema/projectObject.js
@@ -0,0 +1,65 @@
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLList,
+} = require("graphql");
+const Project = require("../models/Project");
+
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    // client: {
+    //   type: ClientType,
+    //   resolve: (parent) => Client.findById(parent.clientId),
+    // },
+  }),
+});
+
+const ProjectQuery = {
+    projects: {
+        type: new GraphQLList(ProjectType),
+        resolve: () => Project.find()
+    },
+    project: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve: (parent, args) => Project.findById(args.id),
+    },
+}
+
+const ProjectMutation = {
+  addProject: {
+    type: ProjectType,
+    args: {
+      name: { type: GraphQLNonNull(GraphQLString) },
+      description: { type: GraphQLString },
+      status: {
+        type: new GraphQLEnumType({
+          name: "ProjectStatus",
+          values: {
+            new: { value: "Not Started" },
+            progress: { value: "In Progress" },
+            completed: { value: "Completed" },
+          },
+        }),
+        defaultValue: "Not Started",
+      },
+      clientId: { type: GraphQLNonNull(GraphQLID) },
+    },
+    resolve: (parent, args) => {
+      const project = new Project(args);
+      return project.save();
+    },
+  },
+};
+
+module.exports = { ProjectType, ProjectMutation, ProjectQuery };
